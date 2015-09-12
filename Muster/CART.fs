95dcaf32ec12071d90e97a715142d81fbb880b6a
Amit Roy <shredderroy@hotmail.com>
2015-09-11 17:55:37 -0700

    Committing the latest changes

--- Muster/CART.fs
+++ Muster/CART.fs
@@ -94,8 +94,6 @@ module CART =
         | Leaf of DataType
         | LeafList of list<DataType>
         | Internal of Map<DataType * DataType, DecisionTreeNode>
-        static member navigate (dt : DecisionTreeNode) (searchKey : DataType * DataType) =
-            []
 
 
     let coreImpurityFn<'A when 'A : equality> (classVals : list<'A>) : list<float> =
