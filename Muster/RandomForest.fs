5d5fe1d23d7bfdbc9779ff06f66a7ee4f520afaf
Amit Roy <shredderroy@hotmail.com>
2015-09-15 13:25:48 -0700

    Committing the latest changes

--- Muster/RandomForest.fs
+++ Muster/RandomForest.fs
@@ -10,5 +10,14 @@ module RandomForest =
     type Forest = list<DecisionTree.Node>
 
 
+    let buildWithParams (tbl : DecisionTree.DataTable) (b : float) : Forest =
+        []
+
+
+    let build (tbl : DecisionTree.DataTable) : Forest =
+        //
+        []
+
+
     let test () : unit = ()
 
