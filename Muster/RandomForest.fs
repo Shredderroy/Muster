175a934b184b2cc07cbf374df80a681d6478e0db
Amit Roy <shredderroy@hotmail.com>
2015-09-15 19:43:35 -0700

    Committing the latest changes

--- Muster/RandomForest.fs
+++ Muster/RandomForest.fs
@@ -10,13 +10,31 @@ module RandomForest =
     type Forest = list<DecisionTree.Node>
 
 
-    type SubsetNum =
+    [<RequireQualifiedAccess>]
+    type SampleSize =
         | Int of int
         | Pct of float
 
 
-    let buildWithParams (tbl : DecisionTree.DataTable) (b : int) (subsetNum : SubsetNum) : Forest =
-        //
+    let errorMsgs =
+        seq [
+            ("sampleSizeErrorMsg", "Invalid specification of SampleSize")]
+        |> Map.ofSeq
+
+
+    let buildWithParams (tbl : DecisionTree.DataTable) (b : int) (sampleSize: SampleSize) : Forest =
+        let sampleSize =
+            match sampleSize with
+            | SampleSize.Int v -> v
+            | SampleSize.Pct p when (p > 0.0) && (p < 1.0) ->
+                tbl
+                |> List.length
+                |> float
+                |> ( * ) p
+                |> Math.Floor
+                |> int
+            | _ -> failwith errorMsgs.["sampleSizeErrorMsg"]
+
         []
 
 
