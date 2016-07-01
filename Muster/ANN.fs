e6e169e35fd25caf8bf47053dcd4912c674f7975
Amit Roy <shredderroy@hotmail.com>
2016-06-30 17:50:47 -0700

    Converted the path to the MKL binary to a relative one

--- Muster/ANN.fs
+++ Muster/ANN.fs
@@ -54,7 +54,7 @@ module ANN =
 
 
     let useNativeMKLProvider () =
-        Control.NativeProviderPath <- @"C:\Users\aroy\OneDrive\Repositories\Muster\packages\MathNet.Numerics.MKL.Win-x64.2.0.0\build\x64"
+        Control.NativeProviderPath <- @"..\..\..\packages\MathNet.Numerics.MKL.Win-x64.2.0.0\build\x64"
         Control.UseNativeMKL()
 
 
