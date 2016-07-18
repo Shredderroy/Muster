71824dea789783d9f91522af8341b223ed0a05bc
SteliosFly <stelios@tachyus.com>
2016-07-17 18:49:11 -0700

    Proper Makefile for compilation

---
+++ Optim/EA2.fs
@@ -0,0 +1,14 @@
+﻿module EA2 
+open System
+let rng= MathNet.Numerics.Distributions.ContinuousUniform(0.0,1.0)
+
+
+
+///Initializes population
+let addddd (nvar:int) (npop:int) =
+    Array.init npop (
+            fun index -> Array.init nvar (
+                            fun ele -> rng.Sample()
+                                         )
+                    )
+
