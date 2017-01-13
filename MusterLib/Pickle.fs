bb21dde2f64f5f390a553406e85b0bf8d6d8a827
Shredderroy <shredderroy@hotmail.com>
2017-01-13 02:12:34 -0800

    Simple addition to the pickler module

--- MusterLib/Pickle.fs
+++ MusterLib/Pickle.fs
@@ -3,20 +3,33 @@
 
 open System
 open System.IO
+open MBrace.FsPickler
 open MBrace.FsPickler.Json
 
 
 module Pickle =
 
 
-    let serialiseData<'A> (dat : 'A) (fileLoc : string) : unit =
+    let serialiseDataJson<'A> (dat : 'A) (fileLoc : string) : unit =
         let jsonSerialiser = FsPickler.CreateJsonSerializer(indent = true)
         let pckl = jsonSerialiser.Pickle dat
         File.WriteAllBytes(fileLoc, pckl)
 
 
-    let deserialiseData<'A> (fileLoc : string) : 'A =
+    let deserialiseDataJson<'A> (fileLoc : string) : 'A =
         let byteArr = File.ReadAllBytes fileLoc
         let jsonSerialiser = FsPickler.CreateJsonSerializer(indent = true)
         jsonSerialiser.UnPickle<'A> byteArr
+
+
+    let serialiseData<'A> (dat : 'A) (fileLoc : string) : unit =
+        let binSerialiser = FsPickler.CreateBinarySerializer()
+        let pckl = binSerialiser.Pickle dat
+        File.WriteAllBytes(fileLoc, pckl)
+
+
+    let deserialiseData<'A> (fileLoc : string) : 'A =
+        let byteArr = File.ReadAllBytes fileLoc
+        let binSerialiser = FsPickler.CreateBinarySerializer()
+        binSerialiser.UnPickle<'A> byteArr
 
