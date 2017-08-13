3304630d0c8b4421ecfc9ae1838b28c2ccd4f7e3
Amit Roy <shredderroy@hotmail.com>
2017-08-12 19:38:16 -0700

    Submitting another correction to CSV.parse, which was being broken by the delimiter at the end of a quoted string

--- MusterLib/CSV.fs
+++ MusterLib/CSV.fs
@@ -10,14 +10,19 @@ module CSV =
 
     let parse (delim : char) (hasHdrs : bool) (lines : #seq<string>) : array<Map<string, string>> =
         let splitLine (line : string) = line.Split [|delim|] |> List.ofArray
+        let q = @""""
+        let f (lst : list<string>) = lst |> (List.rev >> (String.concat (string delim)))
         let parseLine (lst : list<string>) : list<string> =
-            let q = @""""
             (([], []), lst)
             ||> List.fold (fun (s, t) u ->
-                if not (u.EndsWith q) then (u :: s, t)
-                elif not (u.StartsWith q) then ([], ((u :: s) |> List.rev |> String.concat (string delim)) :: t)
-                else ([], u :: t)
-            ) |> snd
+                let v = u.TrimStart()
+                if (v.StartsWith q) && (v.EndsWith q) then
+                    let w = v.Substring(1, max 0 (v.Length - 2))
+                    match s with [] -> ([], w :: t) | _ -> ([], ((w :: s) |> f) :: t)
+                elif (v.StartsWith q) then ((v.Substring 1) :: s, t)
+                elif (v.EndsWith q) then ([], ((u.Substring(0, max 0 (v.Length - 2)) :: s) |> f) :: t)
+                else match s with [] -> [], (u.Trim()) :: t | _ -> u :: s, t
+            ) |> (function s, [] -> s | _, s -> s)
         if lines |> Seq.isEmpty then Array.empty
         else
             lines |> Seq.map (splitLine >> parseLine)
@@ -28,13 +33,13 @@ module CSV =
                 dat |> Seq.map ((List.zip hdrs) >> Map.ofList)
             ) |> Array.ofSeq
 
-
+            
     let parseDefault (lines : #seq<string>) = parse ',' true lines
 
-
+    
     let parseFile (delim : char) (hasHdrs : bool) (filePath : string) : array<Map<string, string>> =
         filePath |> File.ReadAllLines |> parse delim hasHdrs
 
-
+        
     let parseFileDefault (filePath : string) = parseFile ',' true filePath
 
